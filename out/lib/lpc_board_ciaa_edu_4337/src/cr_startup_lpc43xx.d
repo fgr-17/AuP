8a9c8b644dc307f5a8cc59697818c914a2571dbf
fgr-17 <rouxfederico@gmail.com>
2018-04-21 20:08:52 -0300

    Ej 11 SIMD tomando de a un dato de 16 y usando smlad

--- out/lib/lpc_board_ciaa_edu_4337/src/cr_startup_lpc43xx.d
+++
@@ -1,2 +0,0 @@
-out/lib/lpc_board_ciaa_edu_4337/src/cr_startup_lpc43xx.o: \
- lib/lpc_board_ciaa_edu_4337/src/cr_startup_lpc43xx.c
--- out/lib/lpc_board_ciaa_edu_4337/src/crp.d
+++
@@ -1,2 +0,0 @@
-out/lib/lpc_board_ciaa_edu_4337/src/crp.o: \
- lib/lpc_board_ciaa_edu_4337/src/crp.c
